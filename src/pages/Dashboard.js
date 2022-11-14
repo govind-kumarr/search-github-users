fa9ddf5c473a296510928302be6639c7753f326c
govind kumar <gk4051668@gmail.com>
2022-11-14 23:04:03 +0530

    added changes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,6 +3,7 @@ import { Info, Repos, User, Search, Navbar } from "../components";
 import loadingImage from "../images/preloader.gif";
 import { GithubContext } from "../context/context";
 const Dashboard = () => {
+  
   return (
     <main>
       <Navbar></Navbar>
