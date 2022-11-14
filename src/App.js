6f5b3084927d7544ab2df27e7635495edb0d9367
govind kumar <gk4051668@gmail.com>
2022-11-14 22:03:07 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <Route path="/" exact={true}>
-        <Dashboard></Dashboard>
-      </Route>
-      <Route path="/login">
-        <Login></Login>
-      </Route>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="*">
+          <Error></Error>
+        </Route>
+      </Switch>
     </Router>
   );
 }
