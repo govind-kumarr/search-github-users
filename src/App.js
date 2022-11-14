ed5e25d1050a1b3299ca689049f5733d58c4a25d
govind kumar <gk4051668@gmail.com>
2022-11-14 20:32:15 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Route path="/" exact={true}>
+        <Dashboard></Dashboard>
+      </Route>
+      <Route path="/login">
+        <Login></Login>
+      </Route>
+    </Router>
   );
 }
 
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -18,4 +18,5 @@ const Wrapper = styled.section`
     margin-bottom: 1.5rem;
   }
 `;
+
 export default Error;
